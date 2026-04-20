1b3e6700f1b7f22c9a99cd9e2d7f4cd85ebde2e5
mili Arsema <meleymm88@gmail.com>
2026-04-20 05:22:16 -0500

    First outline: skeletal plan

--- src/main/java/com/pluralsight/CarRentalCo.java
+++ src/main/java/com/pluralsight/CarRentalCo.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
 public class CarRentalCo {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Car Rental Co!");
+        //upgrade: which one of our vehicles would you like to rent + list of vehicles
+        // + different rental prices
+
+        System.out.println("To get started, we just need a couple of details from you.");
+        //details
+
+
+
+        //final factors
+
+        double basicCost = 29.99;
+
+    }
+
+  // additional calculations
+    //methods.1: optional costs
+
+    //method.2: surcharge costs
 }
