12fd2993affea62ed712a493f9c7705ff3587252
mili Arsema <meleymm88@gmail.com>
2026-04-20 05:44:52 -0500

    Completed surcharge method

--- src/main/java/com/pluralsight/CarRentalCo.java
+++ src/main/java/com/pluralsight/CarRentalCo.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CarRentalCo {
     public static void main(String[] args) {
 
@@ -9,6 +11,8 @@ public class CarRentalCo {
 
         System.out.println("To get started, we just need a couple of details from you.");
         //details
+        surcharge()
+
 
 
 
@@ -22,4 +26,23 @@ public class CarRentalCo {
     //methods.1: optional costs
 
     //method.2: surcharge costs
+    public static void surcharge(Scanner age) {
+        //build input system
+        Scanner ageInput = new Scanner(System.in);
+        //1: instructions
+        System.out.println("What's your age?");
+        //2: input environment
+        int age = ageInput.nextInt();
+
+        //3: return type
+        if(age > 21 && age < 25){
+            System.out.println("I see you're under 25. You will get extra protection!");
+            double basicCost = 29.99 * 1.3;
+        }else if(age > 25){
+            System.out.println("I see you're over 25. You will be charged at the regular rate");
+            double basicCost = 29.99;
+        }else {
+            System.out.println("Sorry, you can't rent a car as a " + age + " years old");
+        }
+    }
 }
