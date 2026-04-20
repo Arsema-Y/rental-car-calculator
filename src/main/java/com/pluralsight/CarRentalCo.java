5f203e0aed8181a7ec7677fe740ede36c6b43d2b
mili Arsema <meleymm88@gmail.com>
2026-04-20 09:47:33 -0500

    Completed optional cost method with if/else statements. Updated surcharge method

--- src/main/java/com/pluralsight/CarRentalCo.java
+++ src/main/java/com/pluralsight/CarRentalCo.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class CarRentalCo {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Car Rental Co!");
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Welcome to Car Rental Co! \n");
         //upgrade: which one of our vehicles would you like to rent + list of vehicles
         // + different rental prices
 
-        System.out.println("To get started, we just need a couple of details from you.");
+        System.out.println("To get started, we just need a couple of details from you.\n");
+
         //details
-        surcharge()
+        surcharge(keyboard);
 
 
 
@@ -24,9 +27,46 @@ public class CarRentalCo {
 
   // additional calculations
     //methods.1: optional costs
+    public static void optionalCost(Scanner optional){
+        System.out.println("We also have some optional but super helpful additions. \nJust ""Y"" or ""N"" for Yes or No.");
+        //toll tag
+        System.out.println("Electric Toll Tag?");
+        String tollTag = optional.nextLine();
+        double tollTagCost = 0;
+        if(tollTag.equalsIgnoreCase("Y")){
+            tollTagCost = 3.95;
+        }
+
+        //GPS
+        System.out.println("GPS?");
+        String gps = optional.nextLine();
+        double gpsCost = 0;
+        if(gps.equalsIgnoreCase("Y")){
+            gpsCost = 2.95;
+        }
+
+        //Roadside Assist
+        System.out.println("Roadside Assistance?");
+        String roadsideAssistance = optional.nextLine();
+        double roadsideAssistanceCost = 0;
+        if(roadsideAssistance.equalsIgnoreCase("Y")){
+            roadsideAssistanceCost = 3.95;
+        }
+
+        /* output switch
+        switch(tollTag){
+            case "y" -> basicCost += ;
+            case "n" -> basicCost += 0;
+        }
+         */
+
+        //total optional cost
+        double optionalCost = tollTagCost + gpsCost + roadsideAssistanceCost;
+
+    }
 
     //method.2: surcharge costs
-    public static void surcharge(Scanner age) {
+    public static void surcharge(Scanner getAge) {
         //build input system
         Scanner ageInput = new Scanner(System.in);
         //1: instructions
@@ -35,12 +75,12 @@ public class CarRentalCo {
         int age = ageInput.nextInt();
 
         //3: return type
+        double surcharge = 0;
         if(age > 21 && age < 25){
             System.out.println("I see you're under 25. You will get extra protection!");
-            double basicCost = 29.99 * 1.3;
+            surcharge = 29.99 * 0.3;
         }else if(age > 25){
             System.out.println("I see you're over 25. You will be charged at the regular rate");
-            double basicCost = 29.99;
         }else {
             System.out.println("Sorry, you can't rent a car as a " + age + " years old");
         }
