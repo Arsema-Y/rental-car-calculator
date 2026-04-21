96be2d08a41498d08c50c3f906b55488375d1f59
mili Arsema <meleymm88@gmail.com>
2026-04-21 02:00:06 -0500

    Edited for better readability

--- src/main/java/com/pluralsight/CarRentalCo.java
+++ src/main/java/com/pluralsight/CarRentalCo.java
@@ -10,8 +10,10 @@ public class CarRentalCo {
         double basicCost = 29.99;
 
         System.out.println("Welcome to Car Rental Co! \n");
-        //upgrade: which one of our vehicles would you like to rent + list of vehicles
-        // + different rental prices
+        /* upgrade: using Switch Statement~?
+         * which one of our vehicles would you like to rent + list of vehicles
+         * + different rental prices
+         */
 
         System.out.println("Our rental cost start as low as $" + basicCost + "!\n");
         System.out.println("To get started, we just need a couple of details from you.\n");
@@ -26,11 +28,13 @@ public class CarRentalCo {
         double optionalCost = provideOptionalCost(keyboard);
 
         //total fee
-        double totalCost = totalDays * (basicCost + optionalCost + surcharge);
+        double totalCost = (totalDays * (basicCost + optionalCost))+ surcharge;
 
         //display system
         //Receipt
-        System.out.println("\nAwesome! \n\nNow that we have all the details we need, \nbelow is the cost breakdown.\n");
+        System.out.println("\nAwesome! \n\n" +
+                           "Now that we have all the details we need,\n" +
+                           "below is the cost breakdown.\n");
 
         System.out.printf("Vehicle rental $%.2f", (basicCost * totalDays));
         System.out.printf("\nAdditional services: $%.2f", optionalCost);
@@ -42,11 +46,13 @@ public class CarRentalCo {
   // additional calculations
     //methods.1: optional costs
     public static double provideOptionalCost(Scanner optional){
-        System.out.println("\nWe also have some optional but super helpful serices. \nTo apply, Just type (Y) for Yes or (N) for No.\n");
+        System.out.println("\nWe also have some optional but super helpful serices. " +
+                           "\nTo apply, Just type (Y) for Yes or (N) for No.\n");
              optional.nextLine(); //clear buffer
         //toll tag
-        System.out.println("Electric Toll Tag?");
+        System.out.print("Electric Toll Tag: ");
         String tollTag = optional.nextLine();
+
         //toll tag cost
         double tollTagCost = 0;
         if(tollTag.equalsIgnoreCase("Y")){
@@ -54,8 +60,9 @@ public class CarRentalCo {
         }
 
         //GPS
-        System.out.println("GPS?");
+        System.out.print("GPS: ");
         String gps = optional.nextLine();
+
         //GPS cost
         double gpsCost = 0;
         if(gps.equalsIgnoreCase("Y")){
@@ -63,8 +70,9 @@ public class CarRentalCo {
         }
 
         //Roadside Assist
-        System.out.println("Roadside Assistance?");
+        System.out.print("Roadside Assistance: ");
         String roadsideAssistance = optional.nextLine();
+
         //roadside assist cost
         double roadsideAssistanceCost = 0;
         if(roadsideAssistance.equalsIgnoreCase("Y")){
@@ -80,9 +88,9 @@ public class CarRentalCo {
 
         //total optional cost
         double optionalCosts = tollTagCost + gpsCost + roadsideAssistanceCost;
+
         //return
         return optionalCosts;
-
     }
 
     //method.2: surcharge costs
@@ -96,10 +104,12 @@ public class CarRentalCo {
         //3: return type
        double surcharge = 0;
         if(age > 21 && age < 25){
-            System.out.println("I see you're under 25. \nYou will get extra protection!");
+            System.out.println("I see you're under 25. " +
+                               "\nYou will get extra protection!");
             surcharge = 29.99 * 0.3;
         }else if(age >= 25){
-            System.out.println("I see you're over 25. \nYou will be charged at the regular rate");
+            System.out.println("I see you're over 25. " +
+                               "\nYou will be charged at the regular rate");
         }else {
             System.out.println("Sorry, you can't rent a car at " + age);
         }
